5838d1a098923698e50b56ee9d990fc5c78cd8f0
Chris Millar <cmillar@adobe.com>
2021-02-25 12:31:48 -0700

    Add toolbar manifest

---
+++ js/toolbar.js
@@ -0,0 +1,7 @@
+window.frannyfranny = window.frannyfranny || {};
+window.frannyfranny.toolbar = {};
+
+window.frannyfranny.toolbar.changeHeader = () => {
+    const nav = document.querySelector('nav');
+    nav.style.opacity = '0.25';
+};
