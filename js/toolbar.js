dc0e048e23f933b6d185ece9fda54320263a05a3
Chris Millar <cmillar@adobe.com>
2021-02-25 16:56:31 -0700

    remove manifest

--- js/toolbar.js
+++ js/toolbar.js
@@ -1,7 +1,14 @@
-window.frannyfranny = window.frannyfranny || {};
-window.frannyfranny.toolbar = {};
-
-window.frannyfranny.toolbar.changeHeader = () => {
+const changeHeader = () => {
     const nav = document.querySelector('nav');
     nav.style.opacity = '0.25';
+};
+
+window.hlxhlp.toolbar = {
+    styles: '/css/toolbar.css',
+    actions: [
+        {
+            label: 'Change header',
+            action: changeHeader,
+        },
+    ],
 };
